35ac452c4a9476ddc3684a005c1fed3114d0464f
Andrew Meredith <andymeredith@gmail.com>
2021-09-15 13:28:49 -0600

    Updates to Async Rust project

--- rs/async-rust/src/main.rs
+++ rs/async-rust/src/main.rs
@@ -2,13 +2,16 @@ use chrono::prelude::*;
 use clap::Clap;
 use std::io::{Error, ErrorKind};
 use yahoo_finance_api as yahoo;
-use async_trait::async_trait;
+use futures::future::join_all;
+use tokio::time::Duration;
+
+mod signals;
 
 #[derive(Clap)]
 #[clap(
     version = "1.0",
-    author = "Claus Matzinger",
-    about = "A Manning LiveProject: async Rust"
+    author = "Andrew Meredith",
+    about = "A Manning LiveProject implementation: async Rust"
 )]
 struct Opts {
     #[clap(short, long, default_value = "AAPL,MSFT,UBER,GOOG")]
@@ -17,131 +20,6 @@ struct Opts {
     from: String,
 }
 
-///
-/// A trait to provide a common interface for all signal calculations.
-///
-#[async_trait]
-trait AsyncStockSignal {
-
-    ///
-    /// The signal's data type.
-    ///
-    type SignalType;
-
-    ///
-    /// Calculate the signal on the provided series.
-    ///
-    /// # Returns
-    ///
-    /// The signal (using the provided type) or `None` on error/invalid data.
-    ///
-    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType>;
-}
-
-///
-/// Calculates the absolute and relative difference between the beginning and ending of an f64 series. The relative difference is relative to the beginning.
-///
-/// # Returns
-///
-/// A tuple `(absolute, relative)` difference.
-///
-fn price_diff(a: &[f64]) -> Option<(f64, f64)> {
-    if !a.is_empty() {
-        // unwrap is safe here even if first == last
-        let (first, last) = (a.first().unwrap(), a.last().unwrap());
-        let abs_diff = last - first;
-        let first = if *first == 0.0 { 1.0 } else { *first };
-        let rel_diff = abs_diff / first;
-        Some((abs_diff, rel_diff))
-    } else {
-        None
-    }
-}
-
-struct PriceDifference;
-
-#[async_trait]
-impl AsyncStockSignal for PriceDifference {
-    type SignalType = (f64, f64);
-
-    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
-        price_diff(series)
-    }
-}
-
-///
-/// Find the minimum in a series of f64
-///
-fn min(series: &[f64]) -> Option<f64> {
-    if series.is_empty() {
-        None
-    } else {
-        Some(series.iter().fold(f64::MAX, |acc, q| acc.min(*q)))
-    }
-}
-
-struct MinPrice;
-
-#[async_trait]
-impl AsyncStockSignal for MinPrice {
-    type SignalType = f64;
-
-    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
-        min(series)
-    }
-}
-
-
-///
-/// Find the maximum in a series of f64
-///
-fn max(series: &[f64]) -> Option<f64> {
-    if series.is_empty() {
-        None
-    } else {
-        Some(series.iter().fold(f64::MIN, |acc, q| acc.max(*q)))
-    }
-}
-
-struct MaxPrice;
-
-#[async_trait]
-impl AsyncStockSignal for MaxPrice {
-    type SignalType = f64;
-
-    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
-        max(series)
-    }
-}
-
-///
-/// Window function to create a simple moving average
-///
-fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
-    if !series.is_empty() && n > 1 {
-        Some(
-            series
-                .windows(n)
-                .map(|w| w.iter().sum::<f64>() / w.len() as f64)
-                .collect(),
-        )
-    } else {
-        None
-    }
-}
-
-struct WindowedSMA {
-    window_size: usize,
-}
-
-#[async_trait]
-impl AsyncStockSignal for WindowedSMA {
-    type SignalType = Vec<f64>;
-
-    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
-        n_window_sma(self.window_size, series)
-    }
-}
 
 ///
 /// Retrieve data from a data source and extract the closing prices. Errors during download are mapped onto io::Errors as InvalidData.
@@ -167,43 +45,59 @@ async fn fetch_closing_data(
     }
 }
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let opts = Opts::parse();
-    let from: DateTime<Utc> = opts.from.parse().expect("Couldn't parse 'from' date");
-    let to = Utc::now();
-
+async fn fetch_line(symbol: &str, from: DateTime<Utc>, to: DateTime<Utc>) -> std::io::Result<Option<String>> {
     let maxer = MaxPrice;
     let miner = MinPrice;
     let differ = PriceDifference;
     let windower = WindowedSMA { window_size: 30 };
 
-    // a simple way to output a CSV header
-    println!("period start,symbol,price,change %,min,max,30d avg");
-    for symbol in opts.symbols.split(',') {
-        let closes = fetch_closing_data(&symbol, &from, &to).await?;
-        if !closes.is_empty() {
-                // min/max of the period. unwrap() because those are Option types
-                let period_max = maxer.calculate(&closes).await.unwrap();
-                let period_min = miner.calculate(&closes).await.unwrap();
-                let last_price = *closes.last().unwrap_or(&0.0);
-                let (_, pct_change) = differ.calculate(&closes).await.unwrap_or((0.0, 0.0));
-                let sma = windower.calculate(&closes).await.unwrap_or_default();
+    let closes = fetch_closing_data(&symbol, &from, &to).await?;
+    if !closes.is_empty() {
+            // min/max of the period. unwrap() because those are Option types
+            let period_max = maxer.calculate(&closes).await.unwrap();
+            let period_min = miner.calculate(&closes).await.unwrap();
+            let last_price = *closes.last().unwrap_or(&0.0);
+            let (_, pct_change) = differ.calculate(&closes).await.unwrap_or((0.0, 0.0));
+            let sma = windower.calculate(&closes).await.unwrap_or_default();
 
-            // a simple way to output CSV data
-            println!(
-                "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
-                from.to_rfc3339(),
-                symbol,
-                last_price,
-                pct_change * 100.0,
-                period_min,
-                period_max,
-                sma.last().unwrap_or(&0.0)
-            );
+        // a simple way to output CSV data
+        Ok(Some(format!(
+            "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
+            from.to_rfc3339(),
+            symbol,
+            last_price,
+            pct_change * 100.0,
+            period_min,
+            period_max,
+            sma.last().unwrap_or(&0.0)
+        )))
+    } else {
+        Ok(None)
+    }
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let opts = Opts::parse();
+    let from: DateTime<Utc> = opts.from.parse().expect("Couldn't parse 'from' date");
+    let to = Utc::now();
+    let mut interval = tokio::time::interval(Duration::from_secs(30));
+
+    // a simple way to output a CSV header
+    loop {
+        interval.tick().await;
+        println!("period start,symbol,price,change %,min,max,30d avg");
+        let mut futs = Vec::with_capacity(opts.symbols.len());
+        for symbol in opts.symbols.split(',') {
+            futs.push(fetch_line(symbol, from, to));
+        }
+        let lines = futures::future::join_all(futs).await;
+        for line_res in lines {
+            if let Some(line) = line_res? {
+                println!("{}", line)
+            }
         }
     }
-    Ok(())
 }
 
 #[cfg(test)]
---
+++ rs/async-rust/src/signals.rs
@@ -0,0 +1,127 @@
+use async_trait::async_trait;
+
+///
+/// A trait to provide a common interface for all signal calculations.
+///
+#[async_trait]
+trait AsyncStockSignal {
+
+    ///
+    /// The signal's data type.
+    ///
+    type SignalType;
+
+    ///
+    /// Calculate the signal on the provided series.
+    ///
+    /// # Returns
+    ///
+    /// The signal (using the provided type) or `None` on error/invalid data.
+    ///
+    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType>;
+}
+
+///
+/// Calculates the absolute and relative difference between the beginning and ending of an f64 series. The relative difference is relative to the beginning.
+///
+/// # Returns
+///
+/// A tuple `(absolute, relative)` difference.
+///
+fn price_diff(a: &[f64]) -> Option<(f64, f64)> {
+    if !a.is_empty() {
+        // unwrap is safe here even if first == last
+        let (first, last) = (a.first().unwrap(), a.last().unwrap());
+        let abs_diff = last - first;
+        let first = if *first == 0.0 { 1.0 } else { *first };
+        let rel_diff = abs_diff / first;
+        Some((abs_diff, rel_diff))
+    } else {
+        None
+    }
+}
+
+struct PriceDifference;
+
+#[async_trait]
+impl AsyncStockSignal for PriceDifference {
+    type SignalType = (f64, f64);
+
+    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        price_diff(series)
+    }
+}
+
+///
+/// Find the minimum in a series of f64
+///
+fn min(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(f64::MAX, |acc, q| acc.min(*q)))
+    }
+}
+
+struct MinPrice;
+
+#[async_trait]
+impl AsyncStockSignal for MinPrice {
+    type SignalType = f64;
+
+    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        min(series)
+    }
+}
+
+
+///
+/// Find the maximum in a series of f64
+///
+fn max(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(f64::MIN, |acc, q| acc.max(*q)))
+    }
+}
+
+struct MaxPrice;
+
+#[async_trait]
+impl AsyncStockSignal for MaxPrice {
+    type SignalType = f64;
+
+    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        max(series)
+    }
+}
+
+///
+/// Window function to create a simple moving average
+///
+fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
+    if !series.is_empty() && n > 1 {
+        Some(
+            series
+                .windows(n)
+                .map(|w| w.iter().sum::<f64>() / w.len() as f64)
+                .collect(),
+        )
+    } else {
+        None
+    }
+}
+
+struct WindowedSMA {
+    window_size: usize,
+}
+
+#[async_trait]
+impl AsyncStockSignal for WindowedSMA {
+    type SignalType = Vec<f64>;
+
+    async fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        n_window_sma(self.window_size, series)
+    }
+}
